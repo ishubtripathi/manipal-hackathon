6913944d933da8712f50c4cf0b64b64ab55a91fb
ishubtripathi <ishubtripathi@gmail.com>
2025-03-22 12:28:18 +0530

    some changes

---
+++ src/global.d.ts
@@ -0,0 +1,5 @@
+declare module '@codemirror/lang-json';
+declare module '@codemirror/theme-one-dark';
+declare module '@uiw/react-codemirror';
+declare module 'jspdf';
+declare module 'jspdf-autotable';
